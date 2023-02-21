cce8ab8dacb725f76afd3835b80166097ed7da04
HectorSolis <hecsolmay@gmail.com>
2023-02-21 14:05:00 -0600

    Modificando App y config

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:3000/";
+// export const API_URL = "http://localhost:3000/api";
+export const API_URL = "https://betterfood-api.up.railway.app/api";
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Categories from "./pages/categories";
-import Reports from "./pages/reports";
-import Sales from "./pages/sales";
+import Dashboard from "./pages/dashboard";
+import ForgotPassword from "./pages/forgotPassword";
+import Ingredents from "./pages/ingredents";
+import Login from "./pages/login";
 import Notfound from "./pages/notFound";
 import Produts from "./pages/products";
-import Roles from "./pages/roles";
 import Profile from "./pages/profile";
-import Login from "./pages/login";
 import Signup from "./pages/register";
-import ForgotPassword from "./pages/forgotPassword";
-import ProtectedRoutes from "./utils/protectedRoutes";
+import Reports from "./pages/reports";
+import Roles from "./pages/roles";
+import Sales from "./pages/sales";
 import ShoppingList from "./pages/shoppingList";
+import Waiters from "./pages/waiters";
 import MainRoute from "./utils/mainRoute";
+import ProtectedRoutes from "./utils/protectedRoutes";
 
 
 const App = () => {
@@ -29,6 +31,8 @@ const App = () => {
           <Route path="/dashboard" index={true} element={<Dashboard />} />
           <Route path="/products" element={<Produts />} />
           <Route path="/categories" element={<Categories />} />
+          <Route path="/ingredents" element={<Ingredents />} />
+          <Route path="/waiters" element={<Waiters />} />
           <Route path="/reports" element={<Reports />} />
           <Route path="sales" element={<Sales />} />
           <Route path="sales/:id" element={<ShoppingList />} />
