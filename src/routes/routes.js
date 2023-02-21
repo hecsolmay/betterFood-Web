2b7aec07ce4f63006528d38fcf96771b03eb5c0b
HectorSolis <hecsolmay@gmail.com>
2023-02-21 14:03:37 -0600

    Agregando nueva ruta

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,8 @@ export const routes = [
   { text: "Dashboard", icon: "fas fa-fw fa-tachometer-alt", path: "/dashboard" },
   { text: "Products", icon: "fa-solid fa-file", path: "/products" },
   { text: "Categories", icon: "fa-solid fa-bars", path: "/categories" },
+  { text: "Ingredents", icon: "fa-solid fa-bars", path: "/ingredents" },
+  { text: "Waiters", icon: "fa-solid fa-bars", path: "/waiters" },
   { text: "Reports", icon: "fa-solid fa-chart-column", path: "/reports" },
   { text: "Sales", icon: "fa-regular fa-credit-card", path: "/sales" },
   { text: "Roles", icon: "fa-solid fa-users", path: "/roles" },
