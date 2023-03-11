391c5357164aff81a289d8ea3592c656c6ac298f
hecsolmay <hecsolmay@gmail.com>
2023-03-11 01:58:50 -0600

    Cambiando produccion

--- src/App.jsx
+++ src/App.jsx
@@ -12,41 +12,44 @@ import Produts from "./pages/products/";
 import Profile from "./pages/profile/";
 import Signup from "./pages/register/";
 import Reports from "./pages/reports/";
-import ShoppingList from "./pages/orders/details/";
+import ShoppingPage from "./pages/orders/details/";
 import Tables from "./pages/tables/";
 import Roles from "./pages/users/";
 import Waiters from "./pages/waiters/";
 import MainRoute from "./utils/mainRoute";
 import ProtectedRoutes from "./utils/protectedRoutes";
+import { PreviousPageProvider } from "./context/PageContext";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainRoute />} />
+    <PreviousPageProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainRoute />} />
 
-        {/* ADMIN ROUTES */}
-        <Route element={<ProtectedRoutes />}>
-          <Route path="/dashboard" index={true} element={<Dashboard />} />
-          <Route path="/products" element={<Produts />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/ingredents" element={<Ingredents />} />
-          <Route path="/waiters" element={<Waiters />} />
-          <Route path="/tables" element={<Tables />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="orders/:id" element={<ShoppingList />} />
-          <Route path="/roles" element={<Roles />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
+          {/* ADMIN ROUTES */}
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/dashboard" index={true} element={<Dashboard />} />
+            <Route path="/products" element={<Produts />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/ingredents" element={<Ingredents />} />
+            <Route path="/waiters" element={<Waiters />} />
+            <Route path="/tables" element={<Tables />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="orders/:id" element={<ShoppingPage />} />
+            <Route path="/roles" element={<Roles />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
 
-        {/* PUBLIC ROUTES */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </BrowserRouter>
+          {/* PUBLIC ROUTES */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </BrowserRouter>
+    </PreviousPageProvider>
   );
 };
 
