6c3601adc0d4e25fdf257c9930a445d6fc0a43bb
hecsolmay <hecsolmay@gmail.com>
2023-03-11 20:07:21 -0600

    Preparando Produccion

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://127.0.0.1:3000/api";
-// export const API_URL = "https://betterfood-api.up.railway.app/api";
+// export const API_URL = "http://127.0.0.1:3000/api";
+export const API_URL = "https://betterfood-api.up.railway.app/api";
 
