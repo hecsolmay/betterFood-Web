9395a084b566f8aac41187138d2b47c808f33c9c
hecsolmay <hecsolmay@gmail.com>
2023-03-21 23:49:56 -0600

    cambiando url de la api

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // export const API_URL = "http://127.0.0.1:3000/api";
 // export const API_SOCKET_URL = "http://127.0.0.1:3000";
-export const API_SOCKET_URL = "https://betterfood-api.up.railway.app";
-export const API_URL = "https://betterfood-api.up.railway.app/api";
-
+// export const API_SOCKET_URL = "https://betterfood-api.up.railway.app";
+// export const API_URL = "https://betterfood-api.up.railway.app/api";
+export const API_SOCKET_URL = "https://betterfood.onrender.com";
+export const API_URL = "https://betterfood.onrender.com/api";
